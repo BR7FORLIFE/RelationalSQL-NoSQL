9060c04f3c6562be980b61fb1c4fe05354ac8068
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-02-05 12:06:08 -0500

    feat: create the request and response dto for register user

---
+++ projects/mongo-java/src/main/java/com/archives/mongo_java/dtos/request/UserRegisterRequestDto.java
@@ -0,0 +1,5 @@
+package com.archives.mongo_java.dtos.request;
+
+public record UserRegisterRequestDto(String name, String email) {
+
+}
---
+++ projects/mongo-java/src/main/java/com/archives/mongo_java/dtos/response/UserRegisterResponseDto.java
@@ -0,0 +1,5 @@
+package com.archives.mongo_java.dtos.response;
+
+public record UserRegisterResponseDto(String message) {
+
+}
